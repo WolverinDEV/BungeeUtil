d8673ea7c356a83e8087db162b91eb7993d92fbb
essem2 <essem2@users.noreply.github.com>
2016-07-08 13:10:02 +0200

    moved SoundCategory

---
+++ src/dev/wolveringer/api/SoundCategory.java
@@ -0,0 +1,24 @@
+package dev.wolveringer.api;
+
+public enum SoundCategory {
+	MASTER("master"),
+	MUSIC("music"),
+	RECORDS("record"),
+	WEATHER("weather"),
+	BLOCKS("block"),
+	HOSTILE("hostile"),
+	NEUTRAL("neutral"),
+	PLAYERS("player"),
+	AMBIENT("ambient"),
+	VOICE("voice");
+	
+	private String name;
+	
+	private SoundCategory(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
