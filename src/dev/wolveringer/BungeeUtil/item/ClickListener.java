68d96f3e71d005152e993e714ea678075d2c93ac
WolverinDEV <markus.hadenfeldt@web.de>
2016-10-04 20:37:16 +0200

    Removed 1.8 Annonation

--- src/dev/wolveringer/BungeeUtil/item/ClickListener.java
+++ src/dev/wolveringer/BungeeUtil/item/ClickListener.java
@@ -2,7 +2,6 @@ package dev.wolveringer.BungeeUtil.item;
 
 import dev.wolveringer.BungeeUtil.item.ItemStack.Click;
 
-@FunctionalInterface
 public interface ClickListener {
 	public void click(Click click);
 }
