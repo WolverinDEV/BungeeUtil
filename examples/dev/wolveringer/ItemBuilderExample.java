b9f082db12f42ab6f0839045d2a911d67be9d736
WolverinDEV <markus.hadenfeldt@web.de>
2017-02-06 17:12:48 +0100

    Implemented an little example for the ItemBuilder

---
+++ examples/dev/wolveringer/ItemBuilderExample.java
@@ -0,0 +1,34 @@
+package dev.wolveringer;
+
+import dev.wolveringer.bungeeutil.item.Item;
+import dev.wolveringer.bungeeutil.item.ItemBuilder;
+import dev.wolveringer.bungeeutil.item.Material;
+import dev.wolveringer.bungeeutil.item.meta.SkullMeta;
+
+public class ItemBuilderExample {
+	public static Item createItemWithBuilder(){
+		return ItemBuilder.create()
+		        .id(1) //Change the item id
+		        .durbility(0) //Change the item durbility (parameter must be inside Short.MIN_VALUE and Short.MAX_VALUE)
+		        .amount(12) //The amount of the item (maximal Byte.MAX_VALUE minimal Byte.MIN_VALUE)
+		        .name("§aHello world") //Change the item name
+		        .lore("First") //Add a line at the lore
+		        .lore("Second") //Add a second line
+		        .glow() //Let the item glow (the enchant glow)
+		        .listener((click)->{ //Applay a click listener. The variable type of click is ItemStack.Click (Like the general click listener)
+		            click.getPlayer().sendMessage("Hey, you clicked me!"); //Just sending a message to the player who cliked at the item
+		        })
+		        .postListener((builder, item) ->{
+		        	if(item.getType() == Material.SKULL_ITEM){
+		        		SkullMeta meta = (SkullMeta) item.getItemMeta();
+		        		meta.setSkullOwner("WolverinDEV");
+		        		meta.setSkin("WolverinDEV");
+		        	}
+		        	return item;
+		        }) //This methode will be called after the item has been created. 
+		          //You can now apply your own stuff before the create function returns this item.
+		        .build(); //Lets build the item.
+		                  //Return type is an Item, but if the listener has a click listener then
+		                  //its it returned an instance of an ItemStack
+	}
+}
