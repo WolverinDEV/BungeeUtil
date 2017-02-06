2b1bd6b1084d7fa4ed3d4be45c8a085094633ad7
WolverinDEV <markus.hadenfeldt@web.de>
2017-02-06 16:58:46 +0100

    Improved ItemBuilder

--- src/main/java/dev/wolveringer/bungeeutil/item/ItemBuilder.java
+++ src/main/java/dev/wolveringer/bungeeutil/item/ItemBuilder.java
@@ -1,8 +1,19 @@
 package dev.wolveringer.bungeeutil.item;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.Validate;
+
+import lombok.Getter;
+
+@Getter
 public class ItemBuilder {
+	@FunctionalInterface
+	public static interface PostItemBuilder {
+		public Item applay(ItemBuilder builder, Item in);
+	}
+	
 	public static ItemBuilder create() {
 		return new ItemBuilder();
 	}
@@ -19,17 +30,18 @@ public class ItemBuilder {
 		return create(m.getId());
 	}
 
-	private int id;
-	private int sid = -1;
-	private int amouth = -1;
+	private int id = -1;
+	private int metaId = -1;
+	private int amount = -1;
 	private String name;
 	private ArrayList<String> lore = new ArrayList<>();
 	private boolean glow;
 	private ClickListener listener;
 	private Item handle;
 
-	public ItemBuilder() {
-	}
+	private List<PostItemBuilder> postListener = new ArrayList<>();
+	
+	public ItemBuilder() { }
 
 	public ItemBuilder(int id) {
 		this.id = id;
@@ -40,26 +52,27 @@ public class ItemBuilder {
 	}
 
 	public ItemBuilder amount(int n) {
-		this.amouth = n;
+		this.amount = n;
 		return this;
 	}
 
 	@SuppressWarnings("deprecation")
 	public Item build() {
+		Validate.validState(id >= 0, "Invalid item id ("+id+")");
 		Item i;
 		if (this.handle != null) {
 			if (this.id != 0) {
 				this.handle.setTypeId(this.id);
 			}
-			if (this.sid != -1) {
-				this.handle.setDurability((short) this.sid);
+			if (this.metaId != -1) {
+				this.handle.setDurability((short) this.metaId);
 			}
-			if(this.amouth != -1) {
-				this.handle.setAmount(this.amouth);
+			if(this.amount != -1) {
+				this.handle.setAmount(this.amount);
 			}
 			i = this.handle;
 		} else {
-			i = new Item(this.id, this.amouth == -1 ? 1 : this.amouth, (short) (this.sid == -1 ? 0 : this.sid));
+			i = new Item(this.id, this.amount == -1 ? 1 : this.amount, (short) (this.metaId == -1 ? 0 : this.metaId));
 		}
 		if (this.listener != null) {
 			i = new ItemStack(i) {
@@ -78,6 +91,8 @@ public class ItemBuilder {
 		if (this.glow) {
 			i.getItemMeta().setGlow(true);
 		}
+		for(PostItemBuilder postBuilder : postListener)
+			postBuilder.applay(this, i);
 		return i;
 	}
 
@@ -87,7 +102,7 @@ public class ItemBuilder {
 	}
 
 	public ItemBuilder durbility(int short_) {
-		this.sid = short_;
+		this.metaId = short_;
 		return this;
 	}
 
@@ -129,5 +144,24 @@ public class ItemBuilder {
 	public ItemBuilder name(String name) {
 		this.name = name;
 		return this;
+	}
+	
+	public ItemBuilder postListener(PostItemBuilder listener){
+		this.postListener.add(listener);
+		return this;
+	}
+	
+	public ItemBuilder clone(){
+		ItemBuilder _new = new ItemBuilder();
+		_new.amount = this.amount;
+		_new.glow = this.glow;
+		_new.handle = this.handle == null ? null : new Item(this.handle);
+		_new.id = this.id;
+		_new.listener = this.listener;
+		_new.lore = new ArrayList<>(this.lore);
+		_new.name = this.name;
+		_new.postListener = new ArrayList<>(this.postListener);
+		_new.metaId = this.metaId;
+		return _new;
 	}
 }
