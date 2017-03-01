368fd5b9c989feb51249d63e273b196d250d6ce1
WolverinDEV <wolverindev@web.de>
2017-03-01 11:08:01 +0100

    Added message for spigot servers (if users installed this plugin not correct)

--- src/main/java/dev/wolveringer/bungeeutil/plugin/bukkit/BukkitMain.java
+++ src/main/java/dev/wolveringer/bungeeutil/plugin/bukkit/BukkitMain.java
@@ -1,17 +1,16 @@
 package dev.wolveringer.bungeeutil.plugin.bukkit;
 
-/*
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class BukkitMain extends JavaPlugin{
 	@Override
 	public void onEnable() {
-		Bukkit.getConsoleSender().sendMessage(dev.wolveringer.chat.ChatColor.ChatColorUtils.COLOR_CHAR+"7["+dev.wolveringer.chat.ChatColor.ChatColorUtils.COLOR_CHAR+"eBungeeUntil"+dev.wolveringer.chat.ChatColor.ChatColorUtils.COLOR_CHAR+"7] "+dev.wolveringer.chat.ChatColor.ChatColorUtils.COLOR_CHAR+"cBungeeUtil is only a plugin for the BungeeCord");
+		Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY+"[] ----------------------------------------------- []");
+		Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN+"7["+ChatColor.GREEN+"BungeeUntil"+ChatColor.GRAY+"] "+ChatColor.RED+"BungeeUtil is only a plugin for the BungeeCord");
+		Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY+"[] ----------------------------------------------- []");
 	}
-
-}
-*/
-public class BukkitMain  {
 
 }
